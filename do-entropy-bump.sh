3e783118bbbb189cedb806b0c40fd223943c83d5
(no author) <(no author)@d7aec97c-591d-0410-af39-a8856400b30a>
2008-02-25 13:50:46 +0000

    entropy 0.12.23.1 ebuilds
    git-svn-id: http://svn.sabayonlinux.org/overlay@2016 d7aec97c-591d-0410-af39-a8856400b30a

---
+++ do-entropy-bump.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -z "$2" ]; then
+	echo do-entropy-bump.sh OLDVER NEWVER
+	exit
+fi
+
+OLD=$1
+NEW=$2
+PACKAGES="sys-apps/entropy app-admin/equo app-admin/spritz"
+
+for package in ${PACKAGES}; do
+	name=$(echo ${package} | cut -d/ -f2)
+	svn cp ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
+	ebuild ${package}/${name}-${NEW}.ebuild manifest
+done
