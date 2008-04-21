24b9b06af29c3a9af1562dfd4ac06a36a6c81234
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2008-04-21 23:03:56 +0000

    sabayonlive-tools 1.8.0 ebuild
    git-svn-id: http://svn.sabayonlinux.org/overlay@2208 d7aec97c-591d-0410-af39-a8856400b30a

---
+++ app-misc/sabayonlive-tools/files/1.8.0/x-setup-init.d
@@ -0,0 +1,42 @@
+#!/sbin/runscript
+# Copyright 1999-2005 Gentoo Foundation
+# Copyright 2006 SabayonLinux
+# Distributed under the terms of the GNU General Public License v2
+
+depend() {
+	before hostname
+	before xdm
+	after internetkiosk
+}
+
+
+start() {
+
+      is_live=$(cat /proc/cmdline | grep cdroot)
+      if [ -n "$is_live" ]; then
+          ebegin "Configuring GPU Hardware Acceleration and Input devices"
+          start-stop-daemon --start --background --pidfile /var/run/x-setup.pid --make-pidfile --exec /usr/sbin/x-setup-configuration
+          eend 0
+      else
+          ebegin "Configuring GPU Hardware Acceleration and Input devices"
+          if [ -e /first_time_run ] || [ ! -e /etc/gpu-detector.conf ]; then
+              # store config file
+              lspci | grep ' VGA ' > /etc/gpu-detector.conf
+              eend 0
+              return 0
+          fi
+
+          lspci_vga=$(lspci | grep ' VGA ')
+          if [ "$lspci_vga" != "`cat /etc/gpu-detector.conf`" ]; then
+              if [ -e "/sbin/aiglx-setup" ]; then
+                  /sbin/aiglx-setup disable &> /dev/null
+              fi
+              if [ -e "/sbin/xgl-setup" ]; then
+                  /sbin/xgl-setup disable &> /dev/null
+              fi
+              /usr/sbin/x-setup-configuration &> /dev/null
+              lspci | grep ' VGA ' > /etc/gpu-detector.conf
+          fi
+          eend 0
+      fi
+}
