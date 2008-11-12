24193af29f3ac775e6de734a3366cf230d7e114a
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2008-11-12 12:09:48 +0000

    kde-l10n-meta 4.1.3 ebuilds
    git-svn-id: http://svn.sabayonlinux.org/overlay@2595 d7aec97c-591d-0410-af39-a8856400b30a

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for X in `find -name kde-l10n-*4.0.4*.ebuild`; do
+for X in `find -name kde-l10n-*4.0.5*.ebuild`; do
         echo
         echo " ________ DOING "${X}" ________"
         echo
-        svn cp ${X} ${X/4.0.4/4.0.5}
+        svn cp ${X} ${X/4.0.5/4.1.3}
         ebuild ${X} manifest
 done
 
