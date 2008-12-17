792d8c1d7429cd3e4d7933f92dae3633b73a1267
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2008-12-17 12:56:08 +0000

    sabayonlive-tools: version bump, remove input devices autoconfig, reduce xorg.conf size, thx HAL
    git-svn-id: http://svn.sabayonlinux.org/overlay@2687 d7aec97c-591d-0410-af39-a8856400b30a

--- app-misc/sabayonlive-tools/files/1.9.0/x-setup-init.d
+++ app-misc/sabayonlive-tools/files/1.9.0/x-setup-init.d
@@ -12,15 +12,17 @@ depend() {
 start() {
 
       is_live=$(cat /proc/cmdline | grep cdroot)
+      do_redetect=$(cat /proc/cmdline | grep gpudetect)
       if [ -n "$is_live" ]; then
           ebegin "Configuring GPU Hardware Acceleration and Input devices"
           start-stop-daemon --start --background --pidfile /var/run/x-setup.pid --make-pidfile --exec /usr/sbin/x-setup-configuration
           eend 0
       else
-          ebegin "Configuring GPU Hardware Acceleration and Input devices"
-          if [ -e /first_time_run ] || [ ! -e /etc/gpu-detector.conf ]; then
+          ebegin "Configuring GPU Hardware Acceleration and Input devices for the first time"
+          if [ -e /first_time_run ] || [ ! -e /etc/gpu-detector.conf ] || [ -n "$do_redetect" ]; then
               # store config file
               lspci | grep ' VGA ' > /etc/gpu-detector.conf
+              /usr/sbin/x-setup-configuration
               eend 0
               return 0
           fi
