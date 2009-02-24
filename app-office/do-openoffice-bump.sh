23c9190f5889fbe7def4e4860367d0b83f224cc9
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2009-02-24 19:13:57 +0000

    openoffice-l10n: version bump
    git-svn-id: http://svn.sabayonlinux.org/overlay@2790 d7aec97c-591d-0410-af39-a8856400b30a

--- app-office/do-openoffice-bump.sh
+++ app-office/do-openoffice-bump.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-FROM_PV="2.4.1_rc2"
-TO_PV="2.4.1"
-for item in `find -name openoffice-l10n-*.ebuild`; do
+FROM_PV="3.0.0"
+TO_PV="3.0.1"
+for item in `find -name openoffice-l10n-*${FROM_PV}*.ebuild`; do
 
 	echo $item
 	newfile=${item/${FROM_PV}/${TO_PV}}
@@ -11,7 +11,7 @@ for item in `find -name openoffice-l10n-*.ebuild`; do
 	if [ -z "`echo ${item} | grep meta`" ]; then
 		echo "running sed on "${item}
 		# edit
-		sed -i 's/SRC_URI=".*"/SRC_URI="mirror:\/\/openoffice-extended\/\${PV}rc2\/OOo_\${PV}rc2_20080529_LinuxIntel_langpack_\${MY_LANG}.tar.gz"/' ${newfile}
+		sed -i 's/SRC_URI=".*"/SRC_URI="mirror:\/\/openoffice-extended\/\${PV}rc2\/OOo_\${PV}rc2_20090112_LinuxIntel_langpack_\${MY_LANG}.tar.gz"/' ${newfile}
 	fi
 
 	# do manifest
