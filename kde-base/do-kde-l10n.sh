02b3a174afe70fc185de4242fd54083555fd1ee4
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2009-03-05 10:19:19 +0000

    kde-base/kde-l10n-*: version bump
    git-svn-id: http://svn.sabayonlinux.org/overlay@2808 d7aec97c-591d-0410-af39-a8856400b30a

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for X in `find -name kde-l10n-*4.1.3*.ebuild`; do
+for X in `find -name kde-l10n-*4.2.0*.ebuild`; do
         echo
         echo " ________ DOING "${X}" ________"
         echo
-        svn cp ${X} ${X/4.1.3/4.2.0}
+        svn cp ${X} ${X/4.2.0/4.2.1}
         ebuild ${X} manifest
 done
 
