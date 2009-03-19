ab0ff0dacc12d7792ec329999dd25c610c94bdcd
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2009-03-19 15:19:36 +0000

    app-misc/sabayonlive-tools: do not setup xorg.conf.sabayon directly
    git-svn-id: http://svn.sabayonlinux.org/overlay@2855 d7aec97c-591d-0410-af39-a8856400b30a

---
+++ app-misc/sabayonlive-tools/files/2.0.3/sabayon-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sabayon_setup_autologin() {
+
+        # GDM - GNOME
+	if [ -f "/usr/share/gdm/defaults.conf" ]; then
+	        sed -i 's/^AutomaticLoginEnable=.*/AutomaticLoginEnable=true/' /usr/share/gdm/defaults.conf
+        	sed -i 's/^AutomaticLogin=.*/AutomaticLogin=sabayonuser/' /usr/share/gdm/defaults.conf
+	fi
+
+        # KDM - KDE
+        for kver in `ls /usr/kde`; do
+
+                kdm_file="/usr/kde/${kver}/share/config/kdm/kdmrc"
+                if [ ! -f "$kdm_file" ]; then
+                        continue
+                fi
+
+                sed -i 's/AutoLoginEnable=.*/AutoLoginEnable=true/' $kdm_file
+                sed -i 's/AutoLoginUser=.*/AutoLoginUser=sabayonuser/' $kdm_file
+                sed -i 's/AutoLoginDelay=.*/AutoLoginDelay=0/' $kdm_file
+
+                sed -i '/^#.*AutoLoginEnable=/ s/^#//' $kdm_file
+                sed -i '/^#.*AutoLoginUser=/ s/^#//' $kdm_file
+                sed -i '/^#.*AutoLoginDelay=/ s/^#//' $kdm_file
+
+        done
+
+
+}
