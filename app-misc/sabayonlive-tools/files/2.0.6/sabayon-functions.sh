0b40d8508f82d80d7bff193be4b6625bcf9d204a
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2009-03-30 18:40:20 +0000

    sabayonlive-tools: add sabayon-live-check script
    git-svn-id: http://svn.sabayonlinux.org/overlay@2922 d7aec97c-591d-0410-af39-a8856400b30a

---
+++ app-misc/sabayonlive-tools/files/2.0.6/sabayon-functions.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sabayon_setup_autologin() {
+
+        # GDM - GNOME
+	if [ -f "/usr/share/gdm/defaults.conf" ]; then
+	        sed -i 's/^AutomaticLoginEnable=.*/AutomaticLoginEnable=true/' /usr/share/gdm/defaults.conf
+        	sed -i 's/^AutomaticLogin=.*/AutomaticLogin=sabayonuser/' /usr/share/gdm/defaults.conf
+	fi
+
+        # KDM - KDE
+        for kver in `ls /usr/kde`; do
+
+                kdm_file="/usr/kde/${kver}/share/config/kdm/kdmrc"
+                if [ ! -f "$kdm_file" ]; then
+                        continue
+                fi
+
+                sed -i 's/AutoLoginEnable=.*/AutoLoginEnable=true/' $kdm_file
+                sed -i 's/AutoLoginUser=.*/AutoLoginUser=sabayonuser/' $kdm_file
+                sed -i 's/AutoLoginDelay=.*/AutoLoginDelay=0/' $kdm_file
+
+                sed -i '/^#.*AutoLoginEnable=/ s/^#//' $kdm_file
+                sed -i '/^#.*AutoLoginUser=/ s/^#//' $kdm_file
+                sed -i '/^#.*AutoLoginDelay=/ s/^#//' $kdm_file
+
+        done
+
+
+}
+
+sabayon_setup_md_devices() {
+	for i in 0 1 2 3 4 5 6 7 8 9 10; do
+		mknod /dev/md$i b 9 $i &> /dev/null
+	done
+}
