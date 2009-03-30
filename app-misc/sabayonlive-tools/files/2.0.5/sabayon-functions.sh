cbfa04270401322dfbfb01c9407e8bd253d6ad84
lxnay <lxnay@d7aec97c-591d-0410-af39-a8856400b30a>
2009-03-30 18:28:24 +0000

    sabayonlive-tools: mknod to /dev/null
    git-svn-id: http://svn.sabayonlinux.org/overlay@2921 d7aec97c-591d-0410-af39-a8856400b30a

--- app-misc/sabayonlive-tools/files/2.0.5/sabayon-functions.sh
+++ app-misc/sabayonlive-tools/files/2.0.5/sabayon-functions.sh
@@ -30,5 +30,7 @@ sabayon_setup_autologin() {
 }
 
 sabayon_setup_md_devices() {
-	for i in 0 1 2 3 4 5 6 7 8 9 10; do mknod /dev/md$i b 9 $i; done
+	for i in 0 1 2 3 4 5 6 7 8 9 10; do
+		mknod /dev/md$i b 9 $i &> /dev/null
+	done
 }
