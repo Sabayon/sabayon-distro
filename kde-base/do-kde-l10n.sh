e1424b60d5537b77900e039c7f55877787521fb9
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-06-05 09:48:08 +0200

    kde-l10n-* 4.2.4 ebuilds

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for X in `find -name kde-l10n-*4.2.2*.ebuild`; do
+for X in `find -name kde-l10n-*4.2.3*.ebuild`; do
         echo
         echo " ________ DOING "${X}" ________"
         echo
-        svn cp ${X} ${X/4.2.2/4.2.3}
+        cp ${X} ${X/4.2.3/4.2.4}
         ebuild ${X} manifest
 done
 
