f676b3b1f3368acb6ec11e57d09868f880b5983a
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-06-05 10:37:57 +0200

    huge overlay cleanup

--- app-shells/apparix/files/csh.csh
+++
@@ -1,3 +0,0 @@
-alias to    'cd `(apparix -favour rOl \!* || echo -n .)` && pwd'
-alias bm   'apparix --add-mark \!*'
-alias portal 'apparix --add-portal \!*'
--- net-misc/xrdp/files/startwm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# use the gentoo startDM to start the default WM
-#
-# see /usr/share/doc/xrdp-version/startwm.sh for the factory version of this
-# script.
-source /etc/rc.conf
-
-/etc/X11/Sessions/"${XSESSION}"
