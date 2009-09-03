f09fb3f17101d1741f25f8bebbdb96299399db5a
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-09-04 00:34:38 +0200

    [kde-base/kde-l10n-*] add kde-l10n packages for kde 4.3.1

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for X in `find -name kde-l10n-*4.2.95*.ebuild`; do
+for X in `find -name kde-l10n-*4.3.0*.ebuild`; do
         echo
         echo " ________ DOING "${X}" ________"
         echo
-        cp ${X} ${X/4.2.95/4.3.0}
+        cp ${X} ${X/4.3.0/4.3.1}
         ebuild ${X} manifest
 done
 
