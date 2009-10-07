f173eff8568380290e35f9212fe20a07fd4f9e6f
Fabio Erculiani <lxnay@sabayonlinux.org>
2009-10-07 22:13:19 +0200

    [kde-l10n] version bump to 4.3.2

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for X in `find -name kde-l10n-*4.3.0*.ebuild`; do
+for X in `find -name kde-l10n-*4.3.1*.ebuild`; do
         echo
         echo " ________ DOING "${X}" ________"
         echo
-        cp ${X} ${X/4.3.0/4.3.1}
+        cp ${X} ${X/4.3.1/4.3.2}
         ebuild ${X} manifest
 done
 
