9994c833848d539950888b2afab03d0d09b4f0af
Fabio Erculiani <lxnay@sabayon.org>
2010-02-09 13:00:32 +0100

    [kde-base/kde-l10n-*] version bump to 4.4.0

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -8,8 +8,8 @@
 # EXISTING_VER:  An existing version to be used as a template
 # NEW_VER: The new version number to be generated.
 
-EXISTING_VER="4.3.90"
-NEW_VER="4.3.95"
+EXISTING_VER="4.3.95"
+NEW_VER="4.4.0"
 
 for X in `find -name kde-l10n-*${EXISTING_VER}*.ebuild`; do
         echo
