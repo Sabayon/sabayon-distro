3681456710f0c17b6d67441fbd5e458a3eb748e6
Ian Whyman <ian.whyman@sabayon.org>
2010-02-22 21:32:57 +0000

    [script] Add OOo

--- do-artwork-bump.sh
+++ do-artwork-bump.sh
@@ -7,11 +7,12 @@ fi
 OLD=$1
 NEW=$2
 PACKAGES="x11-themes/sabayon-artwork-core x11-themes/sabayon-artwork-extra \
-		x11-themes/sabayon-artwork-kde x11-themes/sabayon-artwork-gnome"
+		x11-themes/sabayon-artwork-kde x11-themes/sabayon-artwork-gnome \
+			x11-themes/sabayon-artwork-ooo"
 
 for package in ${PACKAGES}; do
 	name=$(echo ${package} | cut -d/ -f2)
 	mv ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
 	git add ${package}/${name}-${NEW}.ebuild
-	ebuild ${package}/${name}-${NEW}.ebuild manifest
+	ebuild ${package}/${name}-${NEW}.ebuild manifest --force
 done
