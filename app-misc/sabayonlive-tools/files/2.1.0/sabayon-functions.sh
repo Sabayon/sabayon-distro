b07f63381b7fd69a40de52d09ad234f9dbae380e
Fabio Erculiani <lxnay@sabayon.org>
2010-03-16 19:32:15 +0100

    [app-misc/sabayonlive-tools] revision bump, add extra function, code cleanup

--- app-misc/sabayonlive-tools/files/2.1.0/sabayon-functions.sh
+++ app-misc/sabayonlive-tools/files/2.1.0/sabayon-functions.sh
@@ -2,30 +2,50 @@
 
 sabayon_setup_autologin() {
 
+	local sabayon_user=${SABAYON_USER:-sabayonuser}
+
 	# GDM - GNOME
 	if [ -f "/usr/share/gdm/defaults.conf" ]; then
-		sed -i 's/^AutomaticLoginEnable=.*/AutomaticLoginEnable=true/' /usr/share/gdm/defaults.conf
-		sed -i 's/^AutomaticLogin=.*/AutomaticLogin=sabayonuser/' /usr/share/gdm/defaults.conf
+		sed -i "s/^AutomaticLoginEnable=.*/AutomaticLoginEnable=true/" \
+			/usr/share/gdm/defaults.conf
+		sed -i "s/^AutomaticLogin=.*/AutomaticLogin=${sabayon_user}/" \
+			/usr/share/gdm/defaults.conf
 	fi
 
-        # KDM - KDE
+	# KDM - KDE
 	kdm_file="/usr/share/config/kdm/kdmrc"
 	if [ -f "$kdm_file" ]; then
-		sed -i 's/AutoLoginEnable=.*/AutoLoginEnable=true/' $kdm_file
-		sed -i 's/AutoLoginUser=.*/AutoLoginUser=sabayonuser/' $kdm_file
-		sed -i 's/AutoLoginDelay=.*/AutoLoginDelay=0/' $kdm_file
-	
-		sed -i 's/AllowRootLogin=.*/AllowRootLogin=true/' $kdm_file
-		sed -i 's/AllowNullPasswd=.*/AllowNullPasswd=true/' $kdm_file
-		sed -i 's/AllowShutdown=.*/AllowShutdown=All/' $kdm_file
-	
-		sed -i '/^#.*AutoLoginEnable=/ s/^#//' $kdm_file
-		sed -i '/^#.*AutoLoginUser=/ s/^#//' $kdm_file
-		sed -i '/^#.*AutoLoginDelay=/ s/^#//' $kdm_file
-	
-		sed -i '/^#AllowRootLogin=/ s/^#//' $kdm_file
-		sed -i '/^#AllowNullPasswd=/ s/^#//' $kdm_file
-		sed -i '/^#AllowShutdown=/ s/^#//' $kdm_file
+		sed -i "s/AutoLoginEnable=.*/AutoLoginEnable=true/" $kdm_file
+		sed -i "s/AutoLoginUser=.*/AutoLoginUser=${sabayon_user}" $kdm_file
+		sed -i "s/AutoLoginDelay=.*/AutoLoginDelay=0/" $kdm_file
+
+		sed -i "s/AllowRootLogin=.*/AllowRootLogin=true/" $kdm_file
+		sed -i "s/AllowNullPasswd=.*/AllowNullPasswd=true/" $kdm_file
+		sed -i "s/AllowShutdown=.*/AllowShutdown=All/" $kdm_file
+
+		sed -i "/^#.*AutoLoginEnable=/ s/^#//" $kdm_file
+		sed -i "/^#.*AutoLoginUser=/ s/^#//" $kdm_file
+		sed -i "/^#.*AutoLoginDelay=/ s/^#//" $kdm_file
+
+		sed -i "/^#AllowRootLogin=/ s/^#//" $kdm_file
+		sed -i "/^#AllowNullPasswd=/ s/^#//" $kdm_file
+		sed -i "/^#AllowShutdown=/ s/^#//" $kdm_file
+	fi
+
+}
+
+sabayon_disable_autologin() {
+
+	# GDM - GNOME
+	if [ -f "/usr/share/gdm/defaults.conf" ]; then
+		sed -i "s/^AutomaticLoginEnable=.*/AutomaticLoginEnable=false/" \
+			/usr/share/gdm/defaults.conf
+	fi
+
+	# KDM - KDE
+	kdm_file="/usr/share/config/kdm/kdmrc"
+	if [ -f "$kdm_file" ]; then
+		sed -i "s/AutoLoginEnable=.*/AutoLoginEnable=false/" $kdm_file
 	fi
 
 }
