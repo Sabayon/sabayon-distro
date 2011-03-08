dde81170e3a237df6bc077cabb9e08ce2a4c8a8c
Fabio Erculiani <lxnay@sabayon.org>
2011-03-08 08:15:36 +0100

    [kde-base/kde-l10n-*] version bump to 4.6.1

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # The purpose of this script it to generate new ebuilds for
-# kde l10n pacakges based on using prior version as 
+# kde l10n pacakges based on using prior version as
 # a template.
 #
 # Two variables must be manually supplied to the script:
 # EXISTING_VER:  An existing version to be used as a template
 # NEW_VER: The new version number to be generated.
 
-EXISTING_VER="4.5.5"
-NEW_VER="4.6.0"
+EXISTING_VER="4.6.0"
+NEW_VER="4.6.1"
 BROKEN_ONES=""
 
 for X in `find -name kde-l10n-*${EXISTING_VER}*.ebuild`; do
