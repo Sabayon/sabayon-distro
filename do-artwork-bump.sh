5376d31cd9616b6f15bba063c87559b2663a6c16
Ian Whyman <ian.whyman@sabayon.org>
2011-04-05 21:12:10 +0100

    [x11-themes/sabayon-artwork-*] New 5.6 alpha1, This is just to annoy joost...

--- do-artwork-bump.sh
+++ do-artwork-bump.sh
@@ -8,14 +8,14 @@ OLD=$1
 NEW=$2
 PACKAGES="x11-themes/sabayon-artwork-core x11-themes/sabayon-artwork-extra \
 		x11-themes/sabayon-artwork-kde x11-themes/sabayon-artwork-gnome \
-			x11-themes/sabayon-artwork-ooo x11-themes/sabayon-artwork-lxde"
+			x11-themes/sabayon-artwork-loo x11-themes/sabayon-artwork-lxde"
 
 for package in ${PACKAGES}; do
 	name=$(echo ${package} | cut -d/ -f2)
 	if [ -a ${package}/${name}-${NEW}.ebuild ]; then
 		echo "${NEW} ebuild found, not overwriting"
 	else
-		mv ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
+		cp ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
 	fi
 	git add ${package}/${name}-${NEW}.ebuild
 	ebuild ${package}/${name}-${NEW}.ebuild manifest --force
