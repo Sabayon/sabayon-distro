141569ccce53017bc6a49ddb7347fb20968d9488
Fabio Erculiani <lxnay@sabayon.org>
2011-07-10 19:55:29 +0200

    [sys-kernel/*] revision bump, -r8 wasn't working

---
+++ do-kernel-bump.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ -z "$2" ]; then
+	echo do-kernel-bump.sh OLDVER NEWVER
+	exit
+fi
+
+OLD=$1
+NEW=$2
+PACKAGES="sys-kernel/sabayon-sources sys-kernel/linux-sabayon \
+	sys-kernel/server-sources sys-kernel/linux-server"
+
+for package in ${PACKAGES}; do
+	name=$(echo ${package} | cut -d/ -f2)
+	cp ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
+	git add ${package}/${name}-${NEW}.ebuild
+	ebuild ${package}/${name}-${NEW}.ebuild manifest
+done
