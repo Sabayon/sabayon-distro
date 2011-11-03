b7d41c98cf96947ff1b6811d9369f158b841b4a9
Joost Ruis <joost.ruis@sabayonlinux.org>
2011-11-03 07:18:36 +0000

    [kde-base/kde-l10n*] version bump to 4.7.3

--- kde-base/do-kde-l10n.sh
+++ kde-base/do-kde-l10n.sh
@@ -8,8 +8,8 @@
 # EXISTING_VER:  An existing version to be used as a template
 # NEW_VER: The new version number to be generated.
 
-EXISTING_VER="4.7.1"
-NEW_VER="4.7.2"
+EXISTING_VER="4.7.2"
+NEW_VER="4.7.3"
 BROKEN_ONES=""
 
 for X in `find -name kde-l10n-*${EXISTING_VER}*.ebuild`; do
