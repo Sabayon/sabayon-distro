cb1cba1417e222fb620a36d98862fcc7dd40ce60
Ian Whyman <ian.whyman@sabayon.org>
2012-05-23 00:18:20 +0100

    [x11-themes/sabayon-artwork-*] Shiny new v9 artwork

--- do-artwork-bump.sh
+++ do-artwork-bump.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [ -z "$2" ]; then
 	echo do-artwork-bump.sh OLDVER NEWVER
 	exit
@@ -8,16 +8,17 @@ OLD=$1
 NEW=$2
 PACKAGES="x11-themes/sabayon-artwork-core x11-themes/sabayon-artwork-extra \
 		x11-themes/sabayon-artwork-kde x11-themes/sabayon-artwork-gnome \
-			x11-themes/sabayon-artwork-loo x11-themes/sabayon-artwork-lxde"
+			x11-themes/sabayon-artwork-loo x11-themes/sabayon-artwork-lxde\
+			x11-themes/sabayon-artwork-grub x11-themes/sabayon-artwork-isolinux"
 
 for package in ${PACKAGES}; do
 	name=$(echo ${package} | cut -d/ -f2)
 	if [ -a ${package}/${name}-${NEW}.ebuild ]; then
 		echo "${NEW} ebuild found, not overwriting"
 	else
-		mv ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
+		cp ${package}/${name}-${OLD}.ebuild ${package}/${name}-${NEW}.ebuild
 	fi
-	ebuild ${package}/${name}-${NEW}.ebuild manifest --force
+	ebuild ${package}/${name}-${NEW}.ebuild manifest --force clean install
 	git add ${package}/${name}-${NEW}.ebuild
 	git add ${package}/Manifest
 done
