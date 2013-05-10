37b79e1fa726ee40934a163830724fc396ffaf2f
Fabio Erculiani <lxnay@sabayon.org>
2013-05-10 14:36:51 +0200

    [kde-base/kdm] fix kdm-servercmd.sh, pass "${@}" over

--- kde-base/kdm/files/kdm-servercmd.sh
+++ kde-base/kdm/files/kdm-servercmd.sh
@@ -2,7 +2,7 @@
 
 if [ -e /run/systemd/seats ]; then
 	# logind running
-	exec /usr/lib/systemd/systemd-multi-seat-x %SERVER_ARGS%
+	exec /usr/lib/systemd/systemd-multi-seat-x %SERVER_ARGS% "${@}"
 else
-	exec %SERVER_CMD%
+	exec %SERVER_CMD% "${@}"
 fi
