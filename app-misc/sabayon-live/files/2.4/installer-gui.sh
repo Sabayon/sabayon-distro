0bbce619ba31ac1b247813e5c56f6abc1846f6d9
Fabio Erculiani <lxnay@sabayon.org>
2013-05-28 10:34:34 +0200

    [app-misc/sabayon-live] migrate FILESDIR to git, version bump

--- app-misc/sabayon-live/files/2.4/installer-gui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. /sbin/sabayon-functions.sh
-
-if sabayon_is_gui_install; then
-	sabayon_setup_autologin
-	sabayon_setup_gui_installer
-fi
--- app-misc/sabayon-live/files/2.4/installer-text.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. /sbin/sabayon-functions.sh
-
-if sabayon_is_text_install; then
-	sabayon_setup_text_installer
-fi
