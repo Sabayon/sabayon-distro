4fc4b41cfcaa42c7e2b926fe9db781e024238c77
Fabio Erculiani <lxnay@sabayon.org>
2013-12-23 14:41:54 +0100

    [app-misc/sabayon-mce] remove dead code

--- app-misc/sabayon-mce/files/1.1/libexec/sabayon-mce.sh
+++ app-misc/sabayon-mce/files/1.1/libexec/sabayon-mce.sh
@@ -9,15 +9,8 @@ if sabayon_is_mce; then
     echo "[Desktop]" > /var/sabayonmce/.dmrc
     echo "Session=sabayon-mce" >> /var/sabayonmce/.dmrc
     chown sabayonmce /var/sabayonmce/.dmrc
-    if [ -x "/usr/libexec/gdm-set-default-session" ]; then
-        # TODO: remove this in 6 months
-        # oh my fucking glorious god, this
-        # is AccountsService bullshit
-        # cross fingers
-        /usr/libexec/gdm-set-default-session sabayon-mce
-    fi
+
     if [ -x "/usr/libexec/gdm-set-session" ]; then
-        # GDM 3.6 support
         /usr/libexec/gdm-set-session sabayonmce sabayon-mce
     fi
     sabayon_setup_autologin
@@ -28,4 +21,3 @@ elif ! sabayon_is_live && ! sabayon_is_mce; then
 fi
 
 exit 0
-
