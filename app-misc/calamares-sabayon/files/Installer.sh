6d9c885405cc284451c97a4a17c90a53b4e415f5
mudler <mudler@sabayon.org>
2016-01-21 12:32:58 +0100

    [app-misc/calamares-sabayon-*] revbump

--- app-misc/calamares-sabayon/files/Installer.sh
+++ app-misc/calamares-sabayon/files/Installer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Workaround for locales since locale parsing is tricky in calamares
-cp -rf /etc/locale.gen.bak /etc/locale.gen
+pkexec cp -rf /etc/locale.gen.bak /etc/locale.gen
 calamares-pkexec
