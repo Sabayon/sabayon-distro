8ac0490c6be4259682a60399e7d57e40c9f997da
Francesco Ferro <skullbocks@sabayon.org>
2016-03-16 15:47:55 +0100

    [app-misc/calamares-sabayon-server-base-modules] bumped to 1.1 due calamares new version

--- app-misc/calamares-sabayon-server-base-modules/files/calamares-sabayon-server-base-modules/sabayon/main.py
+++ app-misc/calamares-sabayon-server-base-modules/files/calamares-sabayon-server-base-modules/sabayon/main.py
@@ -27,8 +27,8 @@ def run():
     # Get install path
     install_path = libcalamares.globalstorage.value("rootMountPoint")
     # Grub set background
-    libcalamares.utils.chroot_call(['mkdir', '-p', '/boot/grub/'])
-    libcalamares.utils.chroot_call(['cp', '-f', '/usr/share/grub/default-splash.png', '/boot/grub/default-splash.png'])
+    libcalamares.utils.target_env_call(['mkdir', '-p', '/boot/grub/'])
+    libcalamares.utils.target_env_call(['cp', '-f', '/usr/share/grub/default-splash.png', '/boot/grub/default-splash.png'])
     # Set locales
     locale = libcalamares.globalstorage.value("lcLocale")
     if not locale:
@@ -44,7 +44,7 @@ def run():
         locale_conf.write("LC_MEASUREMENT={!s}\n".format(locale))
         locale_conf.write("LC_MEASUREMENT={!s}\n".format(locale))
         locale_conf.write("LC_COLLATE={!s}\n".format('C'))
-    libcalamares.utils.chroot_call(['env-update'])
+    libcalamares.utils.target_env_call(['env-update'])
 
     return None
 
