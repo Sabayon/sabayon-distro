0a8383bb6e9554f3655ee7360ec2ad1c0d7707b8
mudler <mudler@sabayon.org>
2016-05-12 11:29:14 +0200

    [app-misc/calamares-sabayon-base-modules] bump and fix lightdm gtk-greeter bug

--- app-misc/calamares-sabayon-base-modules/files/calamares-sabayon-base-modules/sabayon/main.py
+++ app-misc/calamares-sabayon-base-modules/files/calamares-sabayon-base-modules/sabayon/main.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# === This file is part of Calamares - <http://github.com/calamares> ===
-#
-#   Calamares is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   Calamares is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import subprocess
@@ -23,7 +9,7 @@ import libcalamares
 import shutil
 
 def run():
-    """ Sabayon Calamares Hack """
+    """ Sabayon Calamares Post-install module"""
     # Get install path
     install_path = libcalamares.globalstorage.value("rootMountPoint")
     # Grub set background
@@ -45,10 +31,12 @@ def run():
         locale_conf.write("LC_MEASUREMENT={!s}\n".format(locale))
         locale_conf.write("LC_COLLATE={!s}\n".format('C'))
     libcalamares.utils.target_env_call(['env-update'])
+
     vbox_fix_path = os.path.join(install_path, "root/vbox_fix.sh")
     with open(vbox_fix_path, "w") as vbox_fix:
         vbox_fix.write("#!/bin/sh\n")
         vbox_fix.write("dmidecode | grep -qvi VirtualBox && systemctl disable virtualbox-guest-additions && systemctl mask virtualbox-guest-additions && rm -rf /etc/xdg/autostart/vboxclient.desktop\n")
+        vbox_fix.write("[ -f \"/etc/lightdm/lightdm.conf\" ] && eselect lightdm set 1\n")
     libcalamares.utils.target_env_call(['sh', '/root/vbox_fix.sh'])
     libcalamares.utils.target_env_call(['rm', '-rf', '/root/vbox_fix.sh'])
 
