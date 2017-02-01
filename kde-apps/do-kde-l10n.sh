d51d8032153f50f7d83a804855527a5eaab5a386
Ettore Di Giacinto <mudler@gentoo.org>
2017-02-01 13:54:19 +0100

    [kde-apps/kde-l10n*] update scripts

--- kde-apps/do-kde-l10n.sh
+++ kde-apps/do-kde-l10n.sh
@@ -8,8 +8,8 @@
 # EXISTING_VER:  An existing version to be used as a template
 # NEW_VER: The new version number to be generated.
 
-EXISTING_VER="16.08.3"
-NEW_VER="16.12.0"
+EXISTING_VER="16.12.0"
+NEW_VER="16.12.1"
 BROKEN_ONES=""
 
 for X in `find -name kde-l10n-*${EXISTING_VER}*.ebuild`; do
