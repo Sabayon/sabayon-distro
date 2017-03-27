423fffe13a00a5aacccb464eac263ad94837f44c
Sławomir Nizio <slawomir.nizio@sabayon.org>
2017-03-27 20:58:56 +0200

    [www-client/firefox-l10n-*] version bump to 52.0.1-r1

--- www-client/ebuilds-new-version-Firefox-l10n.sh
+++ www-client/ebuilds-new-version-Firefox-l10n.sh
@@ -38,7 +38,6 @@ print_ebuild() {
 	cat <<-END
 	# Copyright 1999-2015 Gentoo Foundation
 	# Distributed under the terms of the GNU General Public License v2
-	# \$Id\$
 
 	EAPI=4
 	inherit firefox-l10n
