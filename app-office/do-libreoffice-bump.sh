29cc87e6d2f59509d17d064240e3bac1156098dd
Joost Ruis <joost.ruis@sabayon.org>
2018-02-10 13:30:58 +0100

    [app-office/libreoffice-l10n-*] version bump

--- app-office/do-libreoffice-bump.sh
+++ app-office/do-libreoffice-bump.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-FROM_PV="5.4.0.3"
-TO_PV="5.4.2.2"
+FROM_PV="5.4.2.2"
+TO_PV="6.0.0.3"
 FAILED_LANGS=""
 DONE_LANGS=""
 for item in `find -name libreoffice-l10n-*${FROM_PV}*.ebuild`; do
