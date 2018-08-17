175807c907fc6ff183ad92a76a3cc4b0fa37b0e2
Joost Ruis <joost.ruis@sabayon.org>
2018-08-17 18:07:05 +0200

    [app-office/libreoffice-l10n-*] Bump to version 6.0.2.2 and drop older versions

--- app-office/do-libreoffice-bump.sh
+++ app-office/do-libreoffice-bump.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-FROM_PV="6.0.4.2"
-TO_PV="6.0.5.2"
+FROM_PV="6.0.5.2"
+TO_PV="6.0.6.2"
 FAILED_LANGS=""
 DONE_LANGS=""
 for item in `find -name libreoffice-l10n-*${FROM_PV}*.ebuild`; do
